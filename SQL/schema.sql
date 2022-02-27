0e6c04e66f7f7f1e85c14537ebdd19f5e6ba5b3a
Timothy Smith <timothysmith@Timothys-iMac.local>
2022-02-26 22:51:11 -0500

    Added automated hyperparams testing to model. Added Images folder, SQL folder, Data folder, and Model folder for saved models.

---
+++ SQL/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE mushrooms (
+  edible TEXT,
+  capShape TEXT,
+  capSurface TEXT,
+  capColor TEXT,
+  bruises TEXT,
+  odor TEXT,
+  gillAttachment TEXT,
+  gillSpacing TEXT,
+  gillSize TEXT,
+  gillColor TEXT,
+  stalkShape TEXT,
+  stalkRoot TEXT,
+  stalkSAR TEXT,
+  stalkSBR TEXT,
+  stalkCAR TEXT,
+  stalkCBR TEXT,
+  veilType TEXT,
+  veilColor TEXT,
+  ringNumber TEXT,
+  ringType TEXT,
+  sporePrintColor TEXT,
+  population TEXT,
+  habitat TEXT
+
+);
+
+
